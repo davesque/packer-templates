7b92c5515f21ebde70fadfc90f0eb579ccedcfa9
David Sanders <davesque@gmail.com>
2019-09-25 13:21:34 -0600

    Remove netbsd builder

--- netbsd/add-sudo-shim.netbsd.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-ftp -o master.zip https://github.com/mcandre/posix-sudo-shim/archive/master.zip &&
-    unzip master.zip &&
-    su root -c 'cp "$(pwd)/posix-sudo-shim-master/lib/sudo" /bin/sudo' &&
-    rm -rf posix-sudo-shim-master \
-        master.zip
