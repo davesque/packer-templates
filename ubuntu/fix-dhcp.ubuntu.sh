a45c963228ad4907d0c93ffd00832c701d541144
David Sanders <davesque@gmail.com>
2019-09-25 13:21:22 -0600

    Remove ubuntu builder

--- ubuntu/fix-dhcp.ubuntu.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-printf "#!/bin/sh\ndhclient\nexit 0\n" >>/etc/rc.local &&
-    chmod 0755 /etc/rc.local
--- ubuntu/fix-sudo.ubuntu.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-sed -i -E -e 's/%sudo[[:space:]]+ALL=\(ALL:ALL\)[[:space:]]+ALL/%sudo ALL=(ALL:ALL) NOPASSWD:ALL/' /etc/sudoers
